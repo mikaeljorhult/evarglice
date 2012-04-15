b385ff82f83bcb2be51f63c8b6f2fe3b6cba2e7c
Mikael Jorhult <mikael@jorhult.se>
2012-04-15 14:36:56 +0200

    Inital commit

---
+++ evarglice.js
@@ -0,0 +1,40 @@
+/**
+ * evarglice
+ * minimalistic event delegation.
+ * 
+ * @version 0.1.0
+ * @author Mikael Jorhult
+ * @license http://mikaeljorhult.mit-license.org MIT
+ *
+ * Project repository: https://github.com/mikaeljorhult/evarglice
+ */
+evarglice = {
+	doc: null,
+	event_listen: null,
+	
+	bind: function(type, object, func) {
+		for(var i = 0, length = object.length; i < length; i++) {
+			if(this.event_listen) {
+				object[i].addEventListener(type, func, false);
+			} else {
+				object[i].attachEvent('on' + type, func);
+			}
+		}
+	},
+	
+	unbind: function(type, object, func) {
+		for(var i = 0, length = object.length; i < length; i++) {
+			if(this.event_listen) {
+				object[i].removeEventListener(type, func, false);
+			} else {
+				object[i].detachEvent('on' + type, func);
+			}
+		}
+	},
+	
+	init: function() {
+		this.doc = document;
+		this.event_listen = (this.doc.addEventListener ? true : false);
+		return this;
+	}
+}.init();
