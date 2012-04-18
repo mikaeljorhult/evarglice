bff4fb94bdde8dc49079173000dde62d84e3125d
Mikael Jorhult <mikael@jorhult.se>
2012-04-18 23:05:44 +0200

    Added trigger function

--- evarglice.js
+++ evarglice.js
@@ -24,6 +24,16 @@ evarglice = {
 		return object;
 	},
 	
+	trigger: function(type, object) {
+		for(var i = 0, length = object.length; i < length; i++) {
+			if((object[i][type] || false) && typeof object[i][type] == 'function') {
+				object[i][type](object[i]);
+			}
+		}
+		
+		return object;
+	},
+	
 	unbind: function(type, object, func) {
 		for(var i = 0, length = object.length; i < length; i++) {
 			if(this.event_listen) {
